cb665f8e5fb3e0b91705ca9c90c5e04d482afeb4
GrecoHdz <grecopavonjh@gmail.com>
2026-04-23 00:43:41 -0600

    cambio de version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -155,7 +155,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       appName: 'MiSeguro',
-      appVersion: '1.3.6',
+      appVersion: '1.3.7',
       apiBase: process.env.NUXT_PUBLIC_API_URL
     }
   }
