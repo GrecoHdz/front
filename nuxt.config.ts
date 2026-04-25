bbced06aed650453ae8d1a7955ed2c7962a448ca
GrecoHdz <grecopavonjh@gmail.com>
2026-04-25 17:05:11 -0600

    nueva version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -155,7 +155,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       appName: 'MiSeguro',
-      appVersion: '1.4.2',
+      appVersion: '1.4.3',
       apiBase: process.env.NUXT_PUBLIC_API_URL
     }
   }
